794bb29975414f530282eb9d711dfca34a805248
DoMinhHieu1997 <hieudm112@gmail.com>
2022-04-04 23:26:40 +0700

    multiple upload

--- frontend/src/components/actionModal/index.js
+++ frontend/src/components/actionModal/index.js
@@ -7,15 +7,71 @@ import {
     InputLabel, 
     MenuItem, 
     TextField,
-    TextareaAutosize
+    TextareaAutosize,
+    Button,
+    IconButton,
+    Grid
 } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import { useState } from "react";
 
+const Input = styled('input')({
+    display: 'none',
+});
+
 const ActionModal = (props) => {
+
+    const [imageList, setImageList] = useState([]);
 
     const handleOpen = () => props.setOpenModal(true);
 
     const handleClose = () => props.setOpenModal(false);
+
+    const handleImageUpload = (e) => {
+        const files = e.target.files
+        let reader = new FileReader();
+        function readFile(index) {
+            if( index >= files.length ) return;
+            var file = files[index];
+            reader.onload = function(e) {  
+                // get file content  
+                var bin = e.target.result;
+                console.log(reader.result);
+                setImageList(prev => [...prev,bin]);
+                // do sth with bin
+                readFile(index+1)
+            }
+            reader.readAsBinaryString(file);
+        }
+        readFile(0);
+        // const file = e.target.files;
+        // const filesLength = file.length;
+        // for (let i = 0; i < filesLength; i ++) {
+        //     if (file[i].type === "image/png" || "image/gif" || "image/jpeg") {
+        //         console.log(file[i])
+        //         let reader = new FileReader();
+        //         reader.onload = (event) => {
+        //             console.log(event.target.result);
+        //             console.log(reader.result);
+        //             setImageList(prev => [...prev,reader.result]);
+        //         };
+        //         // file && reader.readAsDataURL(file);
+        //     } else {
+        //         console.log("lỗi upload");
+        //     }
+        // }  
+    }
+
+    const handleRemoveImage = (imageLink) => {
+        const list = [...imageList];
+        const index = list.indexOf(imageLink);
+        if (index > -1) {
+            list.splice(index,1);
+            setImageList(list);
+        }
+    }
 
     return <Modal
         open={props.openModal}
@@ -25,7 +81,7 @@ const ActionModal = (props) => {
     >
         <Box className="top-50 start-50 translate-middle bg-white p-4 position-absolute">
             <Typography id="modal-modal-title" variant="h5" className="fw-bold">
-            Tạo bài viết giới thiệu sản phẩm
+                Tạo bài viết giới thiệu sản phẩm
             </Typography>
             <FormControl fullWidth sx={{ mt:3 }}>
                 <InputLabel id="demo-simple-select-label">Loại sản phẩm</InputLabel>
@@ -47,6 +103,23 @@ const ActionModal = (props) => {
             <Typography id="modal-modal-title" variant="h6" className="fw-bold" sx={{ mt:3 }}>
                 Tải ảnh sản phẩm lên
             </Typography>
+            <label htmlFor="contained-button-file" sx={{ mt:3 }}>
+                <Input accept="image/*" id="contained-button-file" type="file" multiple onChange={handleImageUpload}/>
+                <Button variant="contained" component="span" sx={{ py:1 }}>
+                    <span>Upload</span>
+                    <AddAPhotoIcon fontSize="small" sx={{ml:1, mb:1}}/>
+                </Button>
+            </label>
+            <Grid container sx={{ mt:3 }}>
+                {
+                    imageList.length > 0 && imageList.map((item,index) => {
+                        return <Grid key={index} sx={{ p:2 }} item xs={6} md={4} lg={3} className="position-relative">
+                            <img className="w-100" src={item}/>
+                            <CancelOutlinedIcon className="position-absolute cursor-pointer" style={{ bottom:"95%", left:"95%" }} onClick={() => handleRemoveImage(item)}/>
+                        </Grid>
+                    })
+                }
+            </Grid>
             <TextField id="outlined-basic" label="Kích thước" variant="outlined" sx={{ mt:3 }}/>
             <TextareaAutosize
                 aria-label="empty textarea"
