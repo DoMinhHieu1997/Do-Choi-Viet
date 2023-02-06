0dcccf01bf1f5ec1e352703aa8e8deaedb6d145f
Đỗ Minh Hiếu <minhhieu.do@vietnamnet.vn>
2023-02-06 16:56:24 +0700

    Revert "update"
    This reverts commit 2737b1fe812867bb84530ce5607718d1dae9a9ed.

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL:"https://do-choi-viet-backend.vercel.app/" //http://localhost:5001
+    baseURL:"https://do-choi-viet.herokuapp.com" //http://localhost:5001
 });
 
 // axiosInstance.interceptors.request.use((config) => {
